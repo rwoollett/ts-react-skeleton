32a10927d25656499165da6fc06bb2f403d66605
Rodney Woollett <rodney.woollett@gmail.com>
2023-01-13 17:01:19 +1300

    Console log in server.ts for development NODE_ENV

--- api/server.ts
+++ api/server.ts
@@ -1,19 +1,19 @@
-// External libraries
 import express from 'express';
-
-// Local files
 import defineApi from './api';
-
 import * as BundleEnv from '../env';
 
-// set default ports for webpack env if no ENV port set
-// In mode production the API is on server AND
-// react frontend is static page index.html at path /.
 let port:number = 0;
-if (process.env.PORT) {
-    port = Number.parseInt(process.env.PORT);
-} else if (process.env.NODE_ENV === 'development') {
+console.log(process.env.NODE_ENV);
+console.log('env port', process.env.PORT);
+console.log(BundleEnv.devServer, BundleEnv.devApi, BundleEnv.defaultPort);
+
+// Find local API port
+if (process.env.NODE_ENV === 'development') {
   port = BundleEnv.devApi.port;
+
+} else if (process.env.PORT) {
+    // Production env API portT
+  port = Number.parseInt(process.env.PORT);
 } else {
   // If env has no PORT, we will default to webpack'd env
   port = BundleEnv.defaultPort;
@@ -22,18 +22,22 @@ if (process.env.PORT) {
 var app = express();
 app.use(express.json());
 
-// We serve the bundle folder, which
-// should contain an `index.html` and
-// a `bundle.js` file only.
-//app.use(express.static('dist'));
 app.use('/', express.static('dist'));
 
-// We define the API routes here
 defineApi(app);
 app.get(/.*/, function (req, res) {
   res.sendFile('index.html', { root: __dirname + "./../../dist" });
 })
 
 app.listen(port, function () {
-  console.log('Both front-end and API are up!', port)
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Front-end: ', BundleEnv.devServer.host + ':' + BundleEnv.devServer.port);
+    console.log('API: ',       BundleEnv.devServer.host + ':' + BundleEnv.devApi.port );
+
+  } else {
+    // default is a production mode to run server with bundled js and index.html
+    // along with api served with node server (server.js)
+    //   -- defined in package.json scripts.
+    console.log('Both front-end and API are up!', port)
+  }
 });
